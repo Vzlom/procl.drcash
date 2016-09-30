39534088ac05d027820493c5d2ae873315cc96f9
ivanovvladimirv <development@ivanov.site>
2016-09-30 09:34:24 +0300

    fix

--- yrsa/1kanal/index.php
+++ yrsa/1kanal/index.php
@@ -1,17 +1,17 @@
-<?php include_once("ot.php"); ot_start(); ?>
+<?php //include_once("ot.php"); ot_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Pre-landing</title>
-<?php ot_header();?>
+<?php //ot_header();?>
 </head>
 <body>
-<?php ot_body();?>
+<?php //ot_body();?>
 <h3>Pre-landing</h3>
 <p>Lorem ipsum dolor sit amet, <a href="#">Sale</a> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit <a href="#">Sale</a> in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor <a href="#">Sale</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href="#">Sale</a> incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, <a href="#">Sale</a> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <a href="#">Sale</a> nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui <a href="#">Sale</a> officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <a href="#">Sale</a> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <a href="#">Sale</a> do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation <a href="#">Sale</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, <a href="#">Sale</a> sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 <!-- DrCash -->
-<script type="text/javascript">window.onload = function() { relink('<?php echo ot_add_params("out.php");?>','b.tracking.drcash.me'); metrica(location.search.replace("?","&") + '&host=' + location.host + location.pathname); } </script>
+<script type="text/javascript">window.onload = function() { relink('<?php //echo ot_add_params("out.php");?>','b.tracking.drcash.me'); metrica(location.search.replace("?","&") + '&host=' + location.host + location.pathname); } </script>
 <script src="http://b.cdn.drcash.me/built/assets/js/b.sf.min.js"></script>
 <!-- /DrCash -->   
 </body>
